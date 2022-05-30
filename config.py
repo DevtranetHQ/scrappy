3fcc0e89395f7130235385d5caaf1891f395d668
Emmanuel Katchy <katchyemma@gmail.com>
2022-05-30 15:08:56 +0000

    update config

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 # Read config from environment ("env", "json")
+import json
+from io import StringIO
 
 TOKEN = "MY TOKEN"
 PREFIX = "$"
+
+with open("messages.json") as f:
+    message_data = f.read()
+
+MESSAGES = json.load(StringIO(message_data))
